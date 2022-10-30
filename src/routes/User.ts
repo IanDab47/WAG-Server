500c1d09b45aa18765d07e267c16e433fb040887
brandon-w1205 <bwu1205@gmail.com>
2022-10-30 14:54:11 -0700

    adds score routing

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -6,10 +6,10 @@ import { Schemas, ValidateJoi } from '../middleware/joi'
 const router = express.Router()
 
 router.post('/create', ValidateJoi(Schemas.user.create), controller.createUser)
-// router.post('/create',  controller.createUser)
 router.get('/:userId', controller.getUser)
 router.get('/', controller.getAllUsers)
 router.patch('/:userId', ValidateJoi(Schemas.user.update), controller.updateUser)
+router.patch('/:userId/score', controller.addScore)
 router.delete('/:userId', controller.deleteUser)
 
 export = router
--- typings.d.ts
+++ typings.d.ts
@@ -1,9 +1,14 @@
 import { ObjectId } from "mongoose"
 
+export interface ScoreType {
+  game: string
+  score: number
+}
+
 export interface UserType {
   name: string
   username: string
   email: string
   password: string
-  scores: Array<number>
+  scores: ScoreType[]
 }
