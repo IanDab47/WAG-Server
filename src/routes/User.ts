a63dcd77449db740bac2fff7fe48a008827f3b91
brandon-w1205 <bwu1205@gmail.com>
2022-10-30 17:36:07 -0700

    adds auth locked route example

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import controller from '../controllers/users'
 // import model from '../models/User'
 import { Schemas, ValidateJoi } from '../middleware/joi'
+import authLockedRoute from './authLockedRoute'
 
 const router = express.Router()
 
@@ -12,5 +13,8 @@ router.get('/', controller.getAllUsers)
 router.patch('/:userId', ValidateJoi(Schemas.user.update), controller.updateUser)
 router.patch('/:userId/score', controller.addScore)
 router.delete('/:userId', controller.deleteUser)
+
+// auth-locked route example
+router.get('/auth-locked', authLockedRoute, controller.lockingRouteExample)
 
 export = router
